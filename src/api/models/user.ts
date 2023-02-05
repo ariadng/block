f83bbb350baa954ce3ad80aaecc6ca28100a551b
Aria Dhanang <ariadhanang@gmail.com>
2023-02-06 01:29:15 +0700

    update user by id

--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -34,7 +34,7 @@ export default class UserModel {
 
 	// Create new user
 	public static async store(data: UserInterface) {
-		const password = AES.encrypt(data.password ? data.password : "", getAppKey()).toString();
+		const password = UserModel.encryptPassword(data.password);
 		const user = await prisma.user.create({data: {
 			email: data.email ? data.email : "",
 			name: data.name ? data.name : "",
@@ -68,14 +68,42 @@ export default class UserModel {
 		return user;
 	}
 
+	// Update user by id
+	public static async update(id: number | string, data: UserInterface): Promise<UserInterface|null> {
+		const userId = (typeof id === "string") ? parseInt(id) : id;
+		const userExists = (await UserModel.get(userId)) !== null;
+		if (!userExists) return null;
+		let updated: UserInterface = {};
+		if (typeof data.email !== "undefined" && (await UserModel.emailAvailable(data.email))) updated["email"] = data.email;
+		if (typeof data.password !== "undefined") updated["password"] = UserModel.encryptPassword(data.password);
+		if (typeof data.role !== "undefined") updated["role"] = data.role;
+		if (typeof data.name !== "undefined") updated["name"] = data.name;
+		const user = await prisma.user.update({
+			where: {
+				id: userId,
+			},
+			data: updated,
+		});
+		return user;
+	}
+
 	// Check whether an email has been used
-	public static async emailAvailable(email: string): Promise<boolean> {
+	public static async emailAvailable(email: string, id?: number): Promise<boolean> {
 		const user = await prisma.user.findFirst({
 			where: {
 				email: email,
 			},
 		});
-		return (user === null);
+		if (user === null) return true;
+		if (typeof id === "undefined") return false;
+		if (user.id === id) return true;
+		return false;
+	}
+
+	// Create an encrypted password
+	public static encryptPassword(unencrypted?: string): string {
+		if (typeof unencrypted === "undefined") return "";
+		return AES.encrypt(unencrypted, getAppKey()).toString();
 	}
 
 }
--- src/api/resources/user.resource.ts
+++ src/api/resources/user.resource.ts
@@ -73,4 +73,39 @@ router.get('/:userId', async (req: Request, res: Response) => {
 	});
 });
 
+// Update user by id
+router.put('/:userId', async (req: Request, res: Response) => {
+	const userId = req.params.userId;
+
+	// Check if user exists
+	const userExists = (await UserModel.get(userId)) !== null;
+	if (!userExists) return res.json({
+		status: 404,
+		message: `There is no user with id ${userId}`,
+		data: null,
+	});
+
+	// Get request body
+	const { email, password, role, name } = req.body;
+
+	// Update user
+	const user = await UserModel.update(userId, {
+		email, password, role, name
+	});
+
+	// Failed to update user
+	if (user === null) return res.json({
+		status: 500,
+		message: `Update user with id ${userId} failed`,
+		data: user,
+	});
+
+	// All is well
+	return res.json({
+		status: 200,
+		message: `Update user with id ${userId} success`,
+		data: user,
+	});
+});
+
 export { router as UserRouter };
