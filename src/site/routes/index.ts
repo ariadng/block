7b08fc986daf8dbcea791442e915abf839c1ed70
Aria Dhanang <ariadhanang@gmail.com>
2023-02-06 21:10:51 +0700

    remove cra

--- src/site/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, Router } from "express";
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => res.send("Hello, World!"));
-
-export { router as SiteRouter };
