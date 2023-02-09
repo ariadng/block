9644d6af29539bd8be8b8e462a7c380e71c5c302
Aria Dhanang <ariadhanang@gmail.com>
2023-02-10 02:06:58 +0700

    insert menu items

--- src/app/TypoEditor/TypoInsert.tsx
+++ src/app/TypoEditor/TypoInsert.tsx
@@ -17,6 +17,10 @@ export default function TypoInsert ({
 
 	const [ isOpen, setIsOpen ] = useState<boolean>(false);
 
+	useEffect(() => {
+		if (!show) setIsOpen(false);
+	}, [show])
+
 	return (
 		<div ref={element} className={`TypoInsert ${show ? 'Visible' : 'Invisible'} ${isOpen ? 'Opened' : 'Closed'}`} style={{
 			top: position.top + 5,
@@ -25,6 +29,24 @@ export default function TypoInsert ({
 			<button className="PlusButton" onClick={() => {setIsOpen(!isOpen)}}>
 				<div className="Icon">add</div>
 			</button>
+			<div className="Menu">
+				<div className="button MenuItem">
+					<div className="Icon">image</div>
+					<div className="Label">Image</div>
+				</div>
+				<div className="button MenuItem">
+					<div className="Icon">play_arrow</div>
+					<div className="Label">Video</div>
+				</div>
+				<div className="button MenuItem">
+					<div className="Icon">mic</div>
+					<div className="Label">Audio</div>
+				</div>
+				<div className="button MenuItem">
+					<div className="Icon">data_object</div>
+					<div className="Label">Embed</div>
+				</div>
+			</div>
 		</div>
 	);
 
